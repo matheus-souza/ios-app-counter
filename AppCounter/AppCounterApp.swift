47e5dfee2c646255cf5f24b8c6c50765362d06ba
matheus souza <mh.matheussouza@gmail.com>
2023-04-03 16:29:44 -0300

    Initial Commit

---
+++ AppCounter/AppCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCounterApp.swift
+//  AppCounter
+//
+//  Created by Matheus H. de Souza on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AppCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
