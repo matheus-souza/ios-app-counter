1a98143dd67eae8761e099db72062a1ce49c1c1e
matheus souza <mh.matheussouza@gmail.com>
2023-04-03 18:06:36 -0300

    chore: implemented couter on screen

--- AppCounter/ContentView.swift
+++ AppCounter/ContentView.swift
@@ -7,13 +7,62 @@
 
 import SwiftUI
 
+class Counter : ObservableObject{
+    
+    @Published var days = 0
+    @Published var hours = 0
+    @Published var minutes = 0
+    @Published var seconds = 0
+    
+    init() {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            let calendar = Calendar.current
+            
+            let components = calendar.dateComponents(
+                [.year, .day, .month, .hour, .minute, .second],
+                from: Date()
+            )
+            
+            let currentDate = calendar.date(from: components)
+            
+            var eventDateComponents = DateComponents()
+            eventDateComponents.year = 2023
+            eventDateComponents.month = 12
+            eventDateComponents.day = 25
+            eventDateComponents.hour = 11
+            eventDateComponents.minute = 0
+            eventDateComponents.second = 0
+            
+            
+            let eventDate = calendar.date(from: eventDateComponents)
+            
+            
+            let timeLeft = calendar.dateComponents(
+                [.day, .hour, .minute, .second],
+                from: currentDate!,
+                to: eventDate!
+            )
+            
+            self.days = timeLeft.day ?? 0
+            self.hours = timeLeft.hour ?? 0
+            self.minutes = timeLeft.minute ?? 0
+            self.seconds = timeLeft.second ?? 0
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    @StateObject var counter = Counter()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("\(counter.days) dias")
+                Text("\(counter.hours) horas")
+                Text("\(counter.minutes) min")
+                Text("\(counter.seconds) seg")
+            }
         }
         .padding()
     }
